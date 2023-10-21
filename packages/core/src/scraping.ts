840adc45a78db5de4a3f510b258c56ec78e9cf3b
Felipe Pessina <fs.pessina@gmail.com>
2023-10-21 12:34:56 +0300

    Clean the html

--- packages/core/src/scraping.ts
+++ packages/core/src/scraping.ts
@@ -1,17 +1,115 @@
 import axios from "axios";
 import { parseHTML } from "linkedom";
 import { Readability } from "@mozilla/readability";
+import { Node } from "linkedom/types/interface/node";
 
 export async function fetchPageContent(url: string): Promise<string> {
-  const { data: html } = await axios.get(url);
-  const { window } = parseHTML(html);
-  const document = window.document;
-  const reader = new Readability(document);
-  const article = reader.parse();
+  try {
+    const { data: html } = await axios.get(url);
+    const { window } = parseHTML(html);
+    const document = window.document;
+    const reader = new Readability(document);
+    const article = reader.parse();
 
-  if (article && article.content) {
-    return article.content;
-  } else {
-    throw new Error(`Could not fetch main content from ${url}`);
+    if (article && article.content) {
+      return article.content;
+    } else {
+      throw new Error(`Could not fetch main content from ${url}`);
+    }
+  } catch (error) {
+    console.error(`Error fetching page content: ${error}`);
+    throw error;
   }
 }
+
+export function cleanHTML(html: string): string {
+  try {
+    html = html.replace(/<a[^>]*>([^<]+)<\/a>/g, "$1");
+    html = html.replace(/\s\s+/g, " ").trim();
+    return html;
+  } catch (error) {
+    console.error(`Error cleaning HTML: ${error}`);
+    throw error;
+  }
+}
+
+// interface TextNode {
+//   text: string;
+//   tag: string;
+//   placeholder: string;
+// }
+
+// export function extractTextWithTags(html: string): {
+//   textNodes: TextNode[];
+//   placeholderString: string;
+// } {
+//   const { window } = parseHTML(html);
+//   const document = window.document;
+//   const body = document.body;
+//   const textNodes: TextNode[] = [];
+//   let placeholderString = "";
+
+//   function walk(node: Node, index: number): void {
+//     if (node.nodeName === "#text") {
+//       const placeholder = `{{${index}}}`;
+//       textNodes.push({
+//         text: node.textContent,
+//         tag: node.parentNode.nodeName,
+//         placeholder,
+//       });
+//       placeholderString += ` ${placeholder} `;
+//     } else {
+//       let child = node.firstChild;
+//       while (child) {
+//         walk(child, textNodes.length);
+//         child = child.nextSibling;
+//       }
+//     }
+//   }
+
+//   walk(body, 0);
+//   return { textNodes, placeholderString };
+// }
+
+// export async function translatePlaceholderString(
+//   placeholderString: string,
+//   targetLanguage: string,
+//   translator: (original: string, targetLanguage: string) => string
+// ): Promise<string> {
+//   const translated = await translator(placeholderString, targetLanguage);
+//   return translated;
+// }
+
+// export function reconstructHTMLFromTranslatedPlaceholderString(
+//   translatedPlaceholderString: string,
+//   textNodes: TextNode[]
+// ): string {
+//   let html = translatedPlaceholderString;
+
+//   for (let textObj of textNodes) {
+//     html = html.replace(
+//       textObj.placeholder,
+//       `<${textObj.tag}>${textObj.text}</${textObj.tag}>`
+//     );
+//   }
+
+//   return html;
+// }
+
+// export async function translateHTML(
+//   html: string,
+//   targetLanguage: string,
+//   translator: (original: string, targetLanguage: string) => string
+// ): Promise<string> {
+//   const { textNodes, placeholderString } = extractTextWithTags(html);
+//   const translatedPlaceholderString = await translatePlaceholderString(
+//     placeholderString,
+//     targetLanguage,
+//     translator
+//   );
+//   const translatedHTML = reconstructHTMLFromTranslatedPlaceholderString(
+//     translatedPlaceholderString,
+//     textNodes
+//   );
+//   return translatedHTML;
+// }
--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,14 +1,15 @@
 import { ApiHandler } from "sst/node/api";
-import { fetchPageContent } from "@hublog/core/src/scraping";
+import { cleanHTML, fetchPageContent } from "@hublog/core/src/scraping";
 import { ChatGptService, contentPrompts } from "@hublog/core/src/gpt";
 import { Config } from "sst/node/config";
 
 export const scrapingHandler = ApiHandler(async (evt) => {
   const url = JSON.parse(evt.body ?? "").url;
-  const content = await fetchPageContent(url);
+  const rawContent = await fetchPageContent(url);
+  const cleanContent = cleanHTML(rawContent);
   return {
     statusCode: 200,
-    body: content,
+    body: cleanContent,
   };
 });
 

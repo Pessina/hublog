e15c79ad606137a6c05dd6d7ac23095f6cc3b00e
Pessina <fs.pessina@gmail.com>
2023-11-14 00:23:53 +0400

    gptAPIHandler working

--- stacks/OpenAIStack.ts
+++ stacks/OpenAIStack.ts
@@ -2,12 +2,6 @@ import { Api, Table, Queue, Function } from "sst/constructs";
 import { StackContext } from "sst/constructs";
 
 export function OpenAIStack({ stack }: StackContext) {
-  const api = new Api(stack, "Api", {
-    routes: {
-      "POST /chatgpt": "src/validateAndEnqueue.main",
-    },
-  });
-
   const queue = new Queue(stack, "GPTPromptQueue");
 
   const table = new Table(stack, "ExponentialRetryStatus", {
@@ -17,6 +11,17 @@ export function OpenAIStack({ stack }: StackContext) {
       lastAttemptTime: "string",
     },
     primaryIndex: { partitionKey: "model" },
+  });
+
+  const api = new Api(stack, "Api", {
+    routes: {
+      "POST /chatgpt": {
+        function: {
+          handler: "packages/functions/src/openAIStack.gptAPIHandler",
+          bind: [queue],
+        },
+      },
+    },
   });
 
   stack.addOutputs({
